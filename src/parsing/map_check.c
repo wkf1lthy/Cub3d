fb9f89ad70b0e179908ccc68bf870af73b9c8b48
Valentin Baud <vabaud@c1r5p13.42lehavre.fr>
2025-03-20 16:55:10 +0100

    norm of the player

---
+++ src/parsing/map_check.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_check.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vabaud <vabaud@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 18:34:36 by hbouchel          #+#    #+#             */
+/*   Updated: 2025/03/20 16:52:51 by vabaud           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n')
+		return (1);
+	return (0);
+}
+
+int	is_map_closed(char **map)
+{
+	int		i;
+	int		height;
+	size_t	max_len;
+	size_t	len;
+
+	i = 0;
+	while (map[i])
+		i++;
+	height = i;
+	max_len = 0;
+	i = 0;
+	while (i < height)
+	{
+		len = ft_strlen(map[i]);
+		if (len > max_len)
+			max_len = len;
+		i++;
+	}
+	return (mapping(map, max_len, height));
+}
+
+int	mapping(char **map, size_t max_len, int height)
+{
+	int		i;
+	size_t	j;
+
+	i = 0;
+	while (++i < height)
+	{
+		j = 0;
+		while (++j < max_len)
+		{
+			if (j >= ft_strlen(map[i]))
+			{
+				if (is_border(i, j, height, max_len) && map[i][j] != '1')
+					return (0);
+			}
+			else if (!ft_isspace(map[i][j]) && ((is_border(i, j, height,
+							max_len) && map[i][j] != '1')
+					|| (is_allowed_char(map[i][j]) && is_invalid_space(map, i,
+							j, height))))
+				return (0);
+		}
+	}
+	return (1);
+}
+
+int	forbidden_char(char **map)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (map[++i])
+	{
+		j = -1;
+		while (map[i][++j])
+		{
+			if (!is_allowed_char(map[i][j]) && map[i][j] != '1'
+				&& !ft_isspace(map[i][j]))
+				return (0);
+		}
+	}
+	return (1);
+}
+
+int	check_start_pos(char **map)
+{
+	int	i;
+	int	j;
+	int	start_pos;
+
+	start_pos = 0;
+	i = -1;
+	while (map[++i])
+	{
+		j = -1;
+		while (map[i][++j])
+		{
+			if (map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'W'
+				|| map[i][j] == 'E')
+				start_pos++;
+		}
+	}
+	return (start_pos == 1);
+}
---
+++ src/parsing/path.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbouchel <hbouchel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 18:40:22 by hbouchel          #+#    #+#             */
+/*   Updated: 2025/03/19 18:40:24 by hbouchel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+int	valid_path(char *path, t_all **all, int index)
+{
+	char	*str;
+	int		fd;
+
+	str = ft_strtrim(path, "\n ");
+	if (ft_strstr(str, ".png") == NULL)
+	{
+		free(str);
+		return (0);
+	}
+	fd = open(str, O_RDONLY);
+	if (fd == -1)
+	{
+		free(str);
+		return (0);
+	}
+	close(fd);
+	(*all)->tab_textures[index] = mlx_load_png(str);
+	free(str);
+	if (!(*all)->tab_textures[index])
+		return (0);
+	return (1);
+}
+
+void	update_count(t_all **all, char *id)
+{
+	if (ft_strcmp(id, "NO ") == 0)
+		(*all)->no += 1;
+	else if (ft_strcmp(id, "SO ") == 0)
+		(*all)->so += 1;
+	else if (ft_strcmp(id, "WE ") == 0)
+		(*all)->we += 1;
+	else if (ft_strcmp(id, "EA ") == 0)
+		(*all)->ea += 1;
+	else if (ft_strcmp(id, "F ") == 0)
+		(*all)->f += 1;
+	else if (ft_strcmp(id, "C ") == 0)
+		(*all)->c += 1;
+	else if (ft_strcmp(id, "DO ") == 0)
+		(*all)->d += 1;
+	if ((*all)->c > 1 || (*all)->f > 1 || (*all)->no > 1 || (*all)->so > 1
+		|| (*all)->we > 1 || (*all)->ea > 1 || (*all)->d > 1)
+		ft_all_exit(*all, "Infos are existing more than one time");
+}
+
+int	id_line(char *line, t_all **all, int i)
+{
+	char	*tab[5];
+	int		j;
+	int		k;
+
+	tab[0] = "NO ";
+	tab[1] = "SO ";
+	tab[2] = "EA ";
+	tab[3] = "WE ";
+	tab[4] = "DO ";
+	while (++i < 5)
+	{
+		k = -1;
+		while (line[++k] && line[k] == ' ')
+			;
+		j = -1;
+		while (tab[i][++j] && line && j < 3)
+			if (tab[i][j] != line[k++])
+				break ;
+		if (j == 3)
+			break ;
+	}
+	if (i == 5)
+		return (1);
+	update_count(all, tab[i]);
+	return (0);
+}
+
+int	get_line(char *line)
+{
+	int	i;
+
+	i = -1;
+	while (line[++i] && line[i] == ' ')
+		continue ;
+	if (line[i] == 'N')
+		return (0);
+	else if (line[i] == 'S')
+		return (1);
+	else if (line[i] == 'W')
+		return (2);
+	else if (line[i] == 'E')
+		return (3);
+    else if (line[i] == 'D')
+        return (4);
+	else
+		return (5);
+}
+
+void	is_info_valid(t_all **all, char *line)
+{
+	int	i;
+
+	i = -1;
+	while (line[++i] && line[i] == ' ')
+		continue ;
+	if (!id_line(line, all, -1))
+	{
+		if (!valid_path(&line[i + 2], all, get_line(line)))
+			ft_all_exit(*all, "Informations path is invalid");
+	}
+	else
+	{
+		is_valid_color(all, line);
+		update_color(all, &line[i + 1], line[0]);
+	}
+}
